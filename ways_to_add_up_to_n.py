a4a9d74994ada819e69aa7f9e2e6492290793002
u04rmpeji <78978734+u04rmpeji@users.noreply.github.com>
2021-06-24 11:46:20 +0800

    Upload file

---
+++ ways_to_add_up_to_n.py
@@ -0,0 +1,23 @@
+def ways_to_add_up_to_n(n: int, by: list, stack: list = None):
+    # initialize
+    if not stack:
+        stack = []
+        by = list(set(by))
+        by.sort()
+    this_layer = []
+    previous_layer = []
+    index = 0
+    if n - min(by) >= 0:
+        for number in by:
+            if n - number == 0:
+                this_layer.append(stack + [number])
+            elif n - number < 0:
+                break
+            else:
+                previous_layer.extend(ways_to_add_up_to_n(n - number, by[index:], stack + [number]))
+            index += 1
+    return previous_layer + this_layer
+
+
+if __name__ == "__main__":
+    print(ways_to_add_up_to_n(17, [3, 7, 5, 11]))
